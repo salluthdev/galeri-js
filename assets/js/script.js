8aa1c3c70d312f35c200b2e8822c287d72bc77fa
Faisal L <salluthdev@gmail.com>
2022-05-07 17:25:51 +0700

    Penambahan tombol prev dan next

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,32 @@
+var nomor = 1;
+
 function change(angka){
+	nomor = angka;
 	var lokasi = 'assets/img/gambar-' + angka + '.png';
+	document.getElementById('gambar').src = lokasi;
+	return false;
+}
+
+function prev(){
+	nomor = nomor - 1;
+
+	if(nomor < 1){
+		nomor = 5;
+	}
+
+	var lokasi = 'assets/img/gambar-' + nomor + '.png';
+	document.getElementById('gambar').src = lokasi;
+	return false;
+}
+
+function next(){
+	nomor = nomor + 1;
+
+	if(nomor > 5){
+		nomor = 1;
+	}
+
+	var lokasi = 'assets/img/gambar-' + nomor + '.png';
 	document.getElementById('gambar').src = lokasi;
 	return false;
 }

1baace906bc7bf595dcb0b74e0e9f72204d124fc
Faisal L <salluthdev@gmail.com>
2022-05-08 19:19:43 +0700

    Refactor kode - membuat kode menjadi lebih bersih

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,33 +5,23 @@ var text = ['', 'Footer Growthbar', 'Todo App Design in Pinterest', 'Todo App De
 
 function change(angka){
 	nomor = angka;
-	var lokasi = 'assets/img/gambar-' + angka + '.png';
-	document.getElementById('gambar').src = lokasi;
-	caption.innerHTML = text[angka];
-	return false;
+	ganti_gambar(angka);
 }
 
 function prev(){
-	nomor = nomor - 1;
-
-	if(nomor < 1){
-		nomor = 5;
-	}
-
-	var lokasi = 'assets/img/gambar-' + nomor + '.png';
-	document.getElementById('gambar').src = lokasi;
-	caption.innerHTML = text[nomor];
-	return false;
+	nomor--;
+	if(nomor < 1) nomor = 5;
+	ganti_gambar(nomor);
 }
 
 function next(){
-	nomor = nomor + 1;
+	nomor++;
+	if(nomor > 5) nomor = 1;
+	ganti_gambar(nomor);
+}
 
-	if(nomor > 5){
-		nomor = 1;
-	}
-
-	var lokasi = 'assets/img/gambar-' + nomor + '.png';
+function ganti_gambar(angka){
+	var lokasi = 'assets/img/gambar-' + angka + '.png';
 	document.getElementById('gambar').src = lokasi;
 	caption.innerHTML = text[nomor];
 	return false;
